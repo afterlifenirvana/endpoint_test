72d9da57957fadcd058b2f5c89bb0e0d44cc7c16
Rishi sarswat <rishi@pop-os.localdomain>
2021-05-21 13:18:10 +0530

    changes

---
+++ endpointApi/endpoint_crud/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
+from django.utils import timezone
+from endpoint_crud.models import Endpoint
+from datetime import datetime, timedelta
+
+
+@shared_task(name = "delete_older_endpoints")
+def delete_older_endpoints(*args, **kwargs):
+  now = timezone.now()
+  before = now - timedelta(minutes=5)
+  endpoints = Endpoint.objects.filter(created__lt=before)
+  endpoints.delete()
